bfed1f5ccb7f33983e5716870f287484598b9b00
Fangzhou Xu <axu@Fangzhous-iMac.local>
2018-11-25 00:09:59 -0800

    add meterial ui and cross-fetch

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,5 +1,16 @@
 // @flow
 import * as React from 'react';
+import classNames from 'classnames';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import styles from './App.css';
 
 type Props = {
   children: React.Node
@@ -10,6 +21,50 @@ export default class App extends React.Component<Props> {
 
   render() {
     const { children } = this.props;
-    return <React.Fragment>{children}</React.Fragment>;
+    console.log(this.props);
+
+    const sideList = (
+      <div>
+        <List>
+          {['History', 'Transfer', 'Delegate', 'Undelegate'].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
+        <Divider />
+        <List>
+          {['RAM', 'Voting'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
+
+    return (
+      <div className={styles.root}>
+        <CssBaseline />
+        <Drawer
+          variant="permanent"
+          className={styles.drawer}
+          classes={{ paper: classNames(styles.drawer) }}
+        >
+          {sideList}
+        </Drawer>
+        <main className={styles.content}>
+          <React.Fragment>{children}</React.Fragment>
+        </main>
+      </div>
+    );
   }
 }
