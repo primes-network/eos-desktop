6c54e0b053fff17c6160ecf6c30281d92309f0e4
Fangzhou Xu <axu@Fangzhous-iMac.local>
2018-12-14 22:31:56 -0800

    drawer in account page

--- app/containers/AccountPage.js
+++ app/containers/AccountPage.js
@@ -1,27 +1,96 @@
 // @flow
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
-import routes from '../constants/routes';
+import classNames from 'classnames';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 import AccountInfoContainer from './AccountInfoContainer';
 import AccountActionsContainer from './AccountActionsContainer';
+import routes from '../constants/routes';
 
 type Props = {
-  //   accountName: string
+  classes: object
 };
 
-export default class AccountPage extends Component<Props> {
+const drawerWidth = 220;
+
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing.unit * 3,
+    height: '100vh',
+    overflow: 'auto'
+  }
+});
+const sideList = (
+  <div>
+    <List>
+      {['History', 'Transfer', 'Delegate', 'Undelegate'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+    <Divider />
+    <List>
+      {['RAM', 'Voting'].map((text, index) => (
+        <ListItem button key={text}>
+          <ListItemIcon>
+            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+          </ListItemIcon>
+          <ListItemText primary={text} />
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
+
+class AccountPage extends Component<Props> {
   props: Props;
 
   render() {
-    // const { accountName } = this.props;
+    const { classes } = this.props;
+
     return (
-      <div>
-        <Link to={routes.HOME}>
-          <i className="fa fa-arrow-left fa-3x" />
-        </Link>
-        <AccountInfoContainer accountName="betthefuture" />
-        <AccountActionsContainer accountName="betthefuture" />
+      <div className={classes.root}>
+        <Drawer
+          variant="permanent"
+          classes={{ paper: classNames(classes.drawerPaper) }}
+        >
+          {sideList}
+        </Drawer>
+        <main className={classes.content}>
+          <Link to={routes.HOME}>
+            <i className="fa fa-arrow-left fa-3x" />
+          </Link>
+          <AccountInfoContainer accountName="betthefuture" />
+          <AccountActionsContainer accountName="betthefuture" />
+        </main>
       </div>
     );
   }
 }
+
+export default withStyles(styles)(AccountPage);
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,21 +1,17 @@
 // @flow
 import * as React from 'react';
-import classNames from 'classnames';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import styles from './App.css';
 
 type Props = {
   children: React.Node
 };
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark'
+  }
+});
 
 export default class App extends React.Component<Props> {
   props: Props;
@@ -23,55 +19,10 @@ export default class App extends React.Component<Props> {
   render() {
     const { children } = this.props;
 
-    const sideList = (
-      <div>
-        <List>
-          {['History', 'Transfer', 'Delegate', 'Undelegate'].map(
-            (text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            )
-          )}
-        </List>
-        <Divider />
-        <List>
-          {['RAM', 'Voting'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
-
-    const theme = createMuiTheme({
-      palette: {
-        type: 'dark'
-      }
-    });
-
     return (
       <MuiThemeProvider theme={theme}>
-        <div className={styles.root}>
-          <CssBaseline />
-          <Drawer
-            variant="permanent"
-            className={styles.drawer}
-            classes={{ paper: classNames(styles.drawer) }}
-          >
-            {sideList}
-          </Drawer>
-          <main className={styles.content}>
-            <React.Fragment>{children}</React.Fragment>
-          </main>
-        </div>
+        <CssBaseline />
+        <React.Fragment>{children}</React.Fragment>
       </MuiThemeProvider>
     );
   }
