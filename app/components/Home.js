89108a2c97d1e532ad704dd23e467533b5f3ed30
fzxu <fangzxu@gmail.com>
2019-02-11 23:50:55 -0800

    adjust honme layout

--- app/components/Home.js
+++ app/components/Home.js
@@ -16,8 +16,9 @@ type Props = {
 const styles = () => ({
   root: {
     display: 'flex',
-    flexWrap: 'wrap',
-    textAlign: 'center'
+    flexDirection: 'column',
+    marginTop: '10%',
+    alignItems: 'center'
   }
 });
 
@@ -25,6 +26,19 @@ const EOSNodes = {
   'https://eos.greymass.com': 'operated by greymass',
   // 'https://mainnet.eoscalgary.io': 'operated by EOS Cafe',
   'https://node.eosflare.io': 'operated by EOS Flare'
+  // 'https://api.eosnewyork.io': 'operated by EOS New York',
+  // 'https://api.eosdetroit.io': 'operated by EOS Detroit',
+  // 'https://bp.cryptolions.io': 'operated by CryptoLions',
+  // 'https://api1.eosdublin.io': 'operated by EOS Dublin',
+  // 'https://api2.eosdublin.io': 'operated by EOS Dublin',
+  // 'https://mainnet.eoscannon.io': 'operated by EOS Cannon',
+  // 'https://eos-api.privex.io': 'operated by Privex (privexinceos)',
+  // 'https://eosapi.blockmatrix.network': 'operated by Block Matrix',
+  // 'https://user-api.eoseoul.io': 'operated by EOSeoul',
+  // 'https://api.eos.bitspace.no': 'operated by Bitspace',
+  // 'https://api-eos.blckchnd.com': 'operated by BLCKCHND',
+  // 'https://mainnet.eosimpera.com': 'operated by EOS IMPERA',
+  // 'https://api.franceos.fr': 'operated by franceos'
 };
 
 class Home extends Component<Props> {
@@ -34,10 +48,17 @@ class Home extends Component<Props> {
     super(props);
 
     this.state = {
-      accountName: 'betthefuture'
+      accountName: 'dappinventor'
     };
+
+    this.accountNameInput = React.createRef();
+
     this.handleChange = this.handleChange.bind(this);
     this.handleEOSNodeChange = this.handleEOSNodeChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.accountNameInput.focus();
   }
 
   handleChange(event) {
@@ -60,7 +81,7 @@ class Home extends Component<Props> {
 
     return (
       <div className={classes.root}>
-        <Typography component="h2" gutterBottom>
+        <Typography variant="h3" gutterBottom>
           Primes EOS Desktop
         </Typography>
         <TextField
@@ -84,12 +105,20 @@ class Home extends Component<Props> {
         <TextField
           id="account_name"
           value={accountName}
+          placeholder="Account Name"
           margin="normal"
           variant="outlined"
+          inputRef={ref => {
+            this.accountNameInput = ref;
+          }}
           onChange={this.handleChange}
         />
-        <Button component={Link} to={`/account/${accountName}/history`}>
-          to ACCOUNT
+        <Button
+          variant="contained"
+          component={Link}
+          to={`/account/${accountName}/history`}
+        >
+          Go to Account
         </Button>
       </div>
     );
